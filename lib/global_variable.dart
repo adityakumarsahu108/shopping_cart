b5ac4cb36dcf0fef7f6c01e6c18261f05aecd211
adityakumarsahu108 <adityakumarsahu108@gmail.com>
2024-05-26 18:18:40 +0530

    home page ui and functionality

---
+++ lib/global_variable.dart
@@ -0,0 +1,44 @@
+final products = [
+  {
+    'id': '0',
+    'title': 'Nike Shoes',
+    'prices': 44.52,
+    'sizes': [8, 9, 10, 11],
+    'company': 'Nike',
+    'imageUrl': 'assets/images/shoe1.png',
+  },
+  {
+    'id': '1',
+    'title': 'Addidas Shoes',
+    'prices': 20.52,
+    'sizes': [10, 11],
+    'company': 'Adidas',
+    'imageUrl': 'assets/images/shoe2.png',
+  },
+  {
+    'id': '2',
+    'title': 'Puma Shoes',
+    'prices': 30.52,
+    'sizes': [8, 10, 11],
+    'company': 'Puma',
+    'imageUrl': 'assets/images/shoe3.png',
+  },
+  {
+    'id': '3',
+    'title': 'Campus Shoes',
+    'prices': 48.52,
+    'sizes': [8, 9, 11],
+    'company': 'Campus',
+    'imageUrl': 'assets/images/shoe4.png',
+  },
+];
+final cart = [
+  {
+    'id': '3',
+    'title': 'Campus Shoes',
+    'prices': 48.52,
+    'sizes': [8, 9, 11],
+    'company': 'Campus',
+    'imageUrl': 'assets/images/shoe4.png',
+  },
+];
